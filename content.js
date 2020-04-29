d3bd5dc9c92337363883b6931a60b2ceaa00a58a
djf71 <djf71@pitt.edu>
2020-04-28 20:50:11 -0400

    Fixed URI Reference. Trying to Get Access Token after Autho tab is opened

--- content.js
+++ content.js
@@ -1,9 +1,7 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-   if (request.message === "clicked_browser_action"){
+   if (request.message === "clicked_browser_action"){    
     
-    
-    
-
+    createPlaylist()
 
    }
 })
@@ -22,6 +20,31 @@ function getSongElems(){
     //finally{
     //    alert(song)
     //}
+}
+
+//function to create a Spotify Playlist
+function createPlaylist(){
+
+    const authendpoint = "https://accounts.spotify.com/authorize"
+    const clientId = "a9133a4606a74919aefcc1c43f11247e"
+    const redirectURI = "http://www.google.com"  //'${window.location.protocol}//${window.location.host}/'
+    let query = `client_id=${clientId}&redirect_uri=${redirectURI}&response_type=token`
+    window.location.href = (`${authendpoint}?${query}`)
+     
+    token = window.location.hash.substr(1)
+    console.log(token)
+
+    while (token != "" && !token){
+
+        let token = window.location.hash.substr(1)
+        alert("running")
+
+    }
+    
+    const o = Object.fromEntries(new URLSearchParams(token))
+    //alert(o.access_token)
+    
+
 }
 
 
--- popup.js
+++ popup.js
@@ -8,7 +8,9 @@ document.addEventListener('DOMContentLoaded', function(){
             
             function(tabs){
                 chrome.tabs.sendMessage(tabs[0].id,{"message": "clicked_browser_action"})
-                createPlaylist()
+    
+        
+
             }
                 
             
@@ -19,27 +21,6 @@ document.addEventListener('DOMContentLoaded', function(){
 }, false)
 
  
-
-//function to create a Spotify Playlist
-function createPlaylist(){
-
-    let token = window.location.hash.substr(1)
-    if (token) {
-        const o = Object.fromEntries(new URLSearchParams(token))
-        return o.access_token
-    }else{
-        const authendpoint = "https://accounts.spotify.com/authorize"
-        const clientId = "a9133a4606a74919aefcc1c43f11247e"
-        const redirectURI = "http://localhost:5500"  //'${window.location.protocol}//${window.location.host}/'
-        let query = `client_id=${clientId}&redirect_uri=${redirectURI}&response_type=token`
-        window.open(`${authendpoint}?${query}`)
-
-        console.log(window.location.href) 
-        alert(token)
-    }
-   
-
-}
 
 //function to search song and artist in spotify 
 
