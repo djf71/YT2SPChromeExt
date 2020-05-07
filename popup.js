43c2ade2e76de4e74671a2f58159b1dcedcf1f7b
djf71 <djf71@pitt.edu>
2020-05-06 22:25:15 -0400

    Fixed API Call, Returns JSON

--- popup.js
+++ popup.js
@@ -4,18 +4,12 @@ document.addEventListener('DOMContentLoaded', function(){
     
     function onclick(){
 
-        var client_id = "a9133a4606a74919aefcc1c43f11247e"
-        var redirect_url = chrome.identity.getRedirectURL("spotify")
-        
-        chrome.identity.launchWebAuthFlow({
-            url: `https://accounts.spotify.com/en/authorize?client_id=${client_id}&redirect_uri=${redirect_url}&response_type=token`,
-            interactive : true
-        }, function (redirectURL){
-            var token = redirectURL.substr(redirectURL.indexOf("#")+1).split("&")[0].split("=")[1]
-            
-        
-        })
+        getOAthToken(function(token) {
+            makeReqeust(token);
+        });
 
+        
+        
 
 
 
@@ -35,16 +29,55 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 
-function getOAthURL(){
+function getOAthToken(callback){
 
-    const authendpoint = "https://accounts.spotify.com/authorize"
-    const clientId = "a9133a4606a74919aefcc1c43f11247e"
-    const redirectURI = "http://www.google.com"  //'${window.location.protocol}//${window.location.host}/'
-    let query = `client_id=${clientId}&redirect_uri=${redirectURI}&response_type=token`
+        var client_id = "a9133a4606a74919aefcc1c43f11247e"
+    var redirect_url = chrome.identity.getRedirectURL("spotify")
+    var scopes = "playlist-read-private"
+        
+    chrome.identity.launchWebAuthFlow({
+         url: `https://accounts.spotify.com/en/authorize?client_id=${client_id}&redirect_uri=${redirect_url}&scope=${scopes}&response_type=token`,
+        interactive : true
+    }, function (redirectURL){
+        var token = redirectURL.substr(redirectURL.indexOf("#")+1).split("&")[0].split("=")[1]  
+        console.log(token)
+        callback(token)
     
-     
-    return (`${authendpoint}?${query}`)
+        
+    })
+
+    
 
 }
+
+//testing what making the request looks like
+//will get a list of playlists 
+function makeReqeust(token){
+    
+    fetch("https://api.spotify.com/v1/me/playlists",{
+
+        headers:{
+
+            'Authorization' : 'Bearer ' + token
+            
+        }   
+             
+
+    }).then(response => {
+       return response.json()
+    }).then(response => {
+        title = response.
+        console.log(title)
+    }).catch(response => {
+        console.log("Stupid Error")
+    })
+
+    
+    
+
+
+}
+
+
 
  
