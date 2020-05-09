9f817454362be54e66c5935c1d26e4433c8299e2
David  Fox <dfox@Davids-MacBook-Pro.local>
2020-05-09 09:47:36 -0400

    Created Working Search Function

--- popup.js
+++ popup.js
@@ -10,13 +10,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
         //Gets OAth Token from Spotify and 
         getOAthToken(function(token) {
-           hasYoutubePlaylist(token);
+           addSongToPlaylist(token);
         });
 
         
         chrome.tabs.query({currentWindow: true, active:true}, 
             
             function(tabs){
+                //sends data back to the current tab in chrome 
                 chrome.tabs.sendMessage(tabs[0].id,{"message": "clicked_browser_action"})
 
             }
@@ -29,17 +30,28 @@ document.addEventListener('DOMContentLoaded', function(){
 }, false)
 
 
-
+//function that gets the Spotify OAth token 
+//send the token to a callback function 
 function getOAthToken(callback){
 
+    //Spotify given client ID
+    //From developer website after app registation 
     var client_id = "a9133a4606a74919aefcc1c43f11247e"
+    //Redirect URL per chrome launchWebAuthFlow function
     var redirect_url = chrome.identity.getRedirectURL("spotify")
+    //Scope to allow reading private playlists
     var scopes = "playlist-read-private"
+
         
+    //Launches spotify log in window 
     chrome.identity.launchWebAuthFlow({
          url: `https://accounts.spotify.com/en/authorize?client_id=${client_id}&redirect_uri=${redirect_url}&scope=${scopes}&response_type=token`,
         interactive : true
     }, function (redirectURL){
+
+        
+        //Gets token from the given redirect URL after spotify application 
+        //Isolates token from url 
         var token = redirectURL.substr(redirectURL.indexOf("#")+1).split("&")[0].split("=")[1]  
         
         callback(token)
@@ -51,9 +63,8 @@ function getOAthToken(callback){
 
 }
 
-//testing what making the request looks like
-//will get a list of playlists 
-function hasYoutubePlaylist(token){
+//function to check if there is already a "From Youtube" playlist in the current user's account 
+function addSongToPlaylist(token){
     
     fetch("https://api.spotify.com/v1/me/playlists",{
 
@@ -73,18 +84,47 @@ function hasYoutubePlaylist(token){
             return holder.name
         })
         
-       console.log(titles)
+        //search for song/artist 
+
+        searchForTrack(token)
+
+        //check if there is a "From Youtube" playlist
+
+
+        //If there is not, create one 
+
+        //If there is
+        
+        
 
     }).catch(response => {
-        console.log("Stupid Error")
+        console.log("Error, playlist function undefined")
     })
-
-    
-    
-
 
 }
 
+//searches for song 
+function searchForTrack(token){
 
+    songname = "Danger Zone"
+    artistname = "Kenny Loggins"
 
+    fetch(`https://api.spotify.com/v1/search?q=track:${songname}+artist:${artistname}&type=track`,{
+
+        headers: {
+
+           'Authorization' : 'Bearer ' + token,
+
+        }
+
+    }).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log(response)
+    }).catch(response => {
+        console.log("Error, search undefined")
+    })
+
+    
+}
  
